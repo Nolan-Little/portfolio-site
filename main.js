c3b82bcd4445cca6e0709a1353762df06bfdd43f
Nolan-Little <little.nolan@gmail.com>
2019-01-05 10:22:11 -0600

    complete layout and navbar snapdown and logo links

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let elems = document.querySelectorAll('.sidenav')
-  let options = {
-    draggable: true,
-  }
-  let instances = M.Sidenav.init(elems, options)
-})
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+  $('.scrollspy').scrollSpy();
+});
