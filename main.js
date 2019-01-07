d57c52994cf891db212ed815d24ee3da2f714af2
Nolan-Little <little.nolan@gmail.com>
2019-01-07 14:30:27 -0600

    completed tabs

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.scrollspy').scrollSpy();
 });
+
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
+
+
+// technologies tabs-----------------------------------------
+$('#tool').on("click", () => {
+  $('.tool').removeClass("hidden")
+  $('.code').addClass("hidden")
+})
+
+$('#code').on("click", () => {
+  $('.code').removeClass("hidden")
+  $('.tool').addClass("hidden")
+})
+
+$('#all').on("click", () => {
+  $('.code').removeClass("hidden")
+  $('.tool').removeClass("hidden")
+})
